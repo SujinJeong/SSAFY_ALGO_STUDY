c0686c957eff1372257021b87d03fbaf3cc3d168
KangHoGyun <50542278+KangHoGyun@users.noreply.github.com>
2021-01-28 23:15:42 +0900

    백준 6198

---
+++ src/hogyun/Stack/Solution_6198.java
@@ -0,0 +1,34 @@
+package hogyun.Stack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Solution_6198 {
+	public static int N;
+	public static int []arr; 
+	public static Stack<Integer> st = new Stack<>();
+	public static long sum;
+	
+	public static void getSum() {
+		for(int i=0; i<N; ++i) {
+			while(st.empty() == false && st.peek() <= arr[i]) {
+				st.pop();
+			}
+			st.push(arr[i]);
+			sum += st.size() - 1;
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		arr = new int[N];
+		
+		for(int i=0; i<N; ++i) 
+			arr[i] = sc.nextInt();
+		
+		getSum();
+		System.out.println(sum);
+		sc.close();
+	}
+}
