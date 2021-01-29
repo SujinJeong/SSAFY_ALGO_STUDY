618424b6377552907b3291f7627ea859eb04cb91
hoyeong-s <71427588+hoyeong-s@users.noreply.github.com>
2021-01-30 02:07:52 +0900

    hoyeong

---
+++ src/hoyeong/Stack/back_10799.java
@@ -0,0 +1,47 @@
+package hoyeong.Stack;
+
+import java.util.EmptyStackException;
+import java.util.Scanner;
+import java.util.Stack;
+
+class In{
+	String gual;
+	int cnt;
+	public In(String gual, int cnt) {
+		super();
+		this.gual = gual;
+		this.cnt = cnt;
+	}
+}
+
+public class back_10799 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Stack<In> stack = new Stack<>();
+		String[] arr = sc.next().split("");
+		int sum=0;
+		int LC=0;
+		for(int i=0; i<arr.length;i++) {
+			if(arr[i].equals("(")) stack.push(new In("(",0)); // "(" 일때는 무조건 push
+			else if((arr[i].equals(")"))){												// ")" 일 때
+				try {
+					if(stack.peek().gual.equals("(") && stack.peek().cnt==0) { // 레이저일 때
+						stack.pop();
+						++stack.peek().cnt;
+					}
+					else {
+						sum += stack.peek().cnt+1;
+						LC = stack.peek().cnt;
+						stack.pop();
+						stack.peek().cnt += LC;
+					}
+				} catch (EmptyStackException e) {
+				}
+			}
+			
+		}
+		System.out.println(sum);
+		sc.close();
+	}
+}
