fc09b041e7c842721fd345a5a46397489855863e
hoyeong-s <71427588+hoyeong-s@users.noreply.github.com>
2021-01-31 17:51:50 +0900

    1/31

---
+++ src/hoyeong/Queue/back_11866.java
@@ -0,0 +1,42 @@
+package Queue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class back_11866 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		Queue<Integer> q = new LinkedList<>();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		List<Integer> arr = new ArrayList<>();
+		int K = Integer.parseInt(st.nextToken());
+		
+		for(int i=1; i<=N;i++) {
+			q.add(i);
+		}
+		
+		while(!q.isEmpty()) {
+			for(int j=0; j<K-1; j++) q.add(q.poll());
+			arr.add(q.poll());
+		}
+		
+		System.out.print("<");
+		for(int i=0; i<arr.size();i++) {
+		System.out.print(arr.get(i));
+		if(i==arr.size()-1) break;
+		System.out.print(", ");
+		}
+		System.out.println(">");
+	}
+
+}
---
+++ src/hoyeong/Queue/back_1966.java
@@ -0,0 +1,86 @@
+package Queue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+
+
+class Index implements Comparable<Index>{
+	int idx;
+	int num;
+	public Index(int idx, int num) {
+		super();
+		this.idx = idx;
+		this.num = num;
+	}
+	public int compareTo(Index o) {
+		if(o.num==this.num) return Integer.compare(this.idx,o.idx);
+		return Integer.compare(o.num,this.num);
+	}
+	
+}
+
+public class back_1966 {
+	public static int N;
+	public static int M;
+	static Index[] list;
+	public static int idx;
+	public static int num;
+	public static int cnt;
+	static int [] arr;
+		
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		
+		Queue <Index> q = new LinkedList<>();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		arr = new int[t];
+		for(int i=0; i<t; i++) {
+			cnt=0;
+			q.clear();
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				N = Integer.parseInt(st.nextToken());
+				list = new Index[N];
+				M = Integer.parseInt(st.nextToken());
+				st = new StringTokenizer(br.readLine());
+				for(int j=0; j<N;j++) {
+					num = Integer.parseInt(st.nextToken());
+					q.add(new Index(j,num));
+					list[j] = new Index(j,num);
+					}
+				
+				Arrays.sort(list);
+						
+				for(int k=0; k<N;k++) {
+					while(q.peek().num < list[k].num) {
+						q.add(q.poll());
+					}
+					if(q.peek().idx==M) {cnt++; break;}
+					q.remove(); // 출력
+					cnt++;					
+				}
+			arr[i]=cnt;
+			}
+		br.close();
+		for(int i:arr)
+		System.out.println(i);
+	}
+/*
+3
+1 0
+5
+4 2
+1 2 3 4
+6 0
+1 1 9 1 1 1
+*/
+}
+
