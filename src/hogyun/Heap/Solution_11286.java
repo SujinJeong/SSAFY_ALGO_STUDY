e6889768afd9b1d6b991b64d5ce06f0b5041424e
KangHoGyun <50542278+KangHoGyun@users.noreply.github.com>
2021-01-31 17:15:46 +0900

    절댓값 힙

---
+++ src/hogyun/Heap/Solution_11286.java
@@ -0,0 +1,35 @@
+package hogyun.Heap;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution_11286 {
+	public static int N;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		Queue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+			@Override
+			public int compare(Integer a, Integer b) {
+				int res = (Math.abs(a) == Math.abs(b)) ? a - b : Math.abs(a) - Math.abs(b);
+				return res;
+			}
+		});
+		Queue<Integer> q = new LinkedList<>();
+		int zeroCnt = 0;
+		for (int i = 0; i < N; ++i) {
+			int num = Integer.parseInt(br.readLine());
+			if (num == 0) {
+				zeroCnt++;
+				if(!pq.isEmpty())
+					q.add(pq.poll());
+				else
+					q.add(0);
+			}else
+				pq.offer(num);
+		}
+		for(int i=0; i<zeroCnt; ++i) {
+			System.out.println(q.poll());
+		}
+	}
+}
