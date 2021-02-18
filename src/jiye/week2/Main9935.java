238c1a3ad788d4a3427aec957acc8f8971b731b1
fjwldap <wwldrn927@naver.com>
2021-02-18 09:10:45 +0900

    0217

--- src/jiye/week2/Main9935.java
+++ src/jiye/week2/Main9935.java
@@ -1,27 +1,33 @@
 import java.util.*;
 import java.io.*;
- class Main9935 {
+class Main9935 {//문자열 폭발
     public static void main(String args[]) throws Exception
     {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();//원래 문자열
         String frula = br.readLine();//폭발문자열
+ 
 
         StringBuilder sb = new StringBuilder();
-        if(str.contains(frula)==false)
-            System.out.println(str);
-        else
+        
+        for(int i=0;i<str.length();i++)
         {
-            while(str.contains(frula))
+            sb.append(str.charAt(i));//일단 더하고
+            int index = sb.length();//sb에 마지막 추가한 index
+            if(sb.length()>=frula.length())
             {
-                str=str.replaceAll(frula,"");
+                if(sb.substring(index-frula.length(),index).equals(frula))
+                {//같으면 
+                    sb.delete(index-frula.length(),index);
+                }
             }
+        }
 
-            if(str.length()==0)
+            if(sb.length()==0)
                 System.out.println("FRULA");
             else
-                System.out.println(str);
-        }
+                System.out.println(sb);
+        
     }
     
 }
