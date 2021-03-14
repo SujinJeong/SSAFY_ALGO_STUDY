a5ec310f70700522be2bca58ad874ec515b6858c
hoyeong-s <71427588+hoyeong-s@users.noreply.github.com>
2021-03-14 22:43:45 +0900

    0314

--- src/hoyeong/week7/back_2195.java
+++ src/hoyeong/week7/back_2195.java
@@ -15,13 +15,13 @@ public class back_2195 {
 		
 		int cnt=0, idx=0;
 		outloop: while(idx!=P.length()) {
-			for(int i=S.length()-1; i>=1; i--) {
-				if(idx+i+1>P.length()) continue;
+			for(int i=S.length()-1; i>=1; i--) { // P의 문자열을 앞에서부터 최대 S의 크기만큼 잘라서 생각
+				if(idx+i+1>P.length()) continue; // 예외처리
 								
-				if(S.contains(P.substring(idx, idx+i+1))) {
+				if(S.contains(P.substring(idx, idx+i+1))) { // P의 문자열 중 S내에 존재하는지 비교
 					idx = idx+i+1;
 					cnt++;
-					continue outloop;
+					continue outloop; // 존재한다면 다음 단계
 				}
 			}
 					idx++;
