5e2aa01043975474df768a8500e6ec528a12d1d2
hoyeong-s <71427588+hoyeong-s@users.noreply.github.com>
2021-03-19 01:02:27 +0900

    0319

--- src/hoyeong/week7/back_16890.java
+++ src/hoyeong/week7/back_16890.java
@@ -3,11 +3,17 @@ package hoyeong.week7;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Deque;
 
-public class back_16890 {
+public class back_16890{
 	
 	static char [] apple, love;
+	static Deque<Character> a = new ArrayDeque<>(); 
+	static Deque<Character> l = new ArrayDeque<>(); 
+	static StringBuilder start = new StringBuilder();
+	static StringBuilder end = new StringBuilder();
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,6 +24,7 @@ public class back_16890 {
 		
 		apple = new char[str1.length()];
 		love = new char[str1.length()];
+		int N = str1.length();
 		
 		for(int i=0; i<str1.length(); i++) {
 			apple[i] = str1.charAt(i);
@@ -27,26 +34,27 @@ public class back_16890 {
 		Arrays.sort(apple);
 		Arrays.sort(love);
 		
-		int idx1=0, idx2=str2.length()-1;
+		for(int i=0; i<(N+1)/2; i++)  
+			a.add(apple[i]);
+		for(int j=N-1; j>=(N+1)/2; j--)
+			l.add(love[j]);
 		
-		while (sb.length() != str1.length()) {
-			if (sb.length() + 2 <= str1.length()) {
-				if (apple[idx1] > love[idx2]) {
-					sb.append(love[idx2]);
-					if (sb.length() == str1.length())  break;
-					sb.append(apple[idx1]);
-				} else {
-					sb.append(apple[idx1]);
-					if (sb.length() == str1.length())  break;
-					sb.append(love[idx2]);
+		for(int i=0; i<N; i++) {
+			if(i%2 == 0) { // 구사과
+				if(l.isEmpty() || a.peek() < l.peek()) {
+					start.append(a.pollFirst());
 				}
-				idx1++;
-				idx2--;
+				else end.append(a.pollLast());
 			}
-			else {
-				sb.append(apple[idx1]);
+			else { // 큐브러버
+				if(a.isEmpty()||a.peek() < l.peek()) { // 비정상적인 상황
+					start.append(l.pollFirst());
+				}
+				else end.append(l.pollLast());
 			}
 		}
-		System.out.println(sb.toString());
+		System.out.print(start);
+		for(int i=end.length()-1; i>=0; i--)
+			System.out.print(end.charAt(i));
 	}
 }
