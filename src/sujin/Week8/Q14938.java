b4d8b8f5551fa4ad574bab01543648bf7ffdc5ef
SujinJeong <64065318+SujinJeong@users.noreply.github.com>
2021-03-23 20:24:34 +0900

    Q14938 서강그라운드 -- 수진

--- src/sujin/Week8/Q14938.java
+++ src/sujin/Week8/Q14938.java
@@ -3,36 +3,79 @@ package sujin.Week8;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Q14938 {
+	static int n, m, r;
+	static int[][] adj;
+	static int[] item;
+
 	public static void init() {
-		
+		for (int i = 1; i <= n; i++)
+			for (int j = 1; j <= n; j++) {
+				if (i == j)
+					continue;
+				if (adj[i][j] == 0)
+					adj[i][j] = 987654321;
+			}
 	}
+
+	public static void floyd() {
+
+		// k : 거쳐가는 노드
+		for (int k = 1; k <= n; k++)
+			// i : 출발 노드
+			for (int i = 1; i <= n; i++)
+				// j : 도착 노드
+				for (int j = 1; j <= n; j++)
+					if (adj[i][k] + adj[k][j] < adj[i][j])
+						adj[i][j] = adj[i][k] + adj[k][j];
+
+	}
+
+	public static void calMax() {
+		int max = 0;
+
+		for (int i = 1; i <= n; i++) {
+			int sum = 0;
+			for (int j = 1; j <= n; j++) {
+				if (adj[i][j] <= m) {
+					sum += item[j];
+				}
+			}
+			max = Math.max(sum, max);
+		}
+
+		System.out.println(max);
+	}
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] line = br.readLine().split(" ");
+		StringTokenizer st;
 
-		int n = Integer.parseInt(line[0]);
-		int m = Integer.parseInt(line[1]);
-		int r = Integer.parseInt(line[2]);
-		int[] item = new int[n+1];
-		int[][] adj = new int[r][r];
-		
-		// 아이템 input
-		line = br.readLine().split(" ");
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		r = Integer.parseInt(st.nextToken());
+		item = new int[n + 1];
+		adj = new int[n + 1][n + 1];
+
+		st = new StringTokenizer(br.readLine());
 		for (int i = 1; i <= n; i++) {
-			item[i] = Integer.parseInt(line[i-1]);
+			item[i] = Integer.parseInt(st.nextToken());
 		}
-		
-		Arrays.fill(adj, Integer.MAX_VALUE);
-		
-		for (int i = 0; i < r; i++) {
-			line = br.readLine().split(" ");
-			//adj[]
-		}
-		
-		
-	}
 
+		for (int i = 0; i < r; i++) {
+			st = new StringTokenizer(br.readLine());
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			int weight = Integer.parseInt(st.nextToken());
+			adj[from][to] = weight;
+			adj[to][from] = weight;
+		}
+
+		init();
+		floyd();
+		calMax();
+	}
 }
