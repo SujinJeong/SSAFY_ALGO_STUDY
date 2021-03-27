93e8e39c70c0ffb456875860ed29d4bb8c8a17f8
hoyeong-s <71427588+hoyeong-s@users.noreply.github.com>
2021-03-27 23:23:47 +0900

    0327

--- src/hoyeong/week3/back_9466.java
+++ src/hoyeong/week3/back_9466.java
@@ -1,21 +1,16 @@
-package week3;
+package hoyeong.week3;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class back_9466 {
-	static int N, cnt;
+	static int N, cnt,INF=987654321,team;
 	static int[] arr;
-	static boolean visited[];
-	static boolean team[];
-	static List<Integer> list = new ArrayList<>();
+	static int[][] cycle;
+	static boolean [] visited;
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,39 +18,47 @@ public class back_9466 {
 		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
 		
-		for(int i=0; i<T; i++) {
+		for(int t=0; t<T; t++) {
 			
 			N = Integer.parseInt(br.readLine());
 			visited = new boolean[N+1];
-			team = new boolean[N+1];
 			arr = new int[N+1]; 
+			cycle = new int[2][N+1]; 
 			
 			st = new StringTokenizer(br.readLine());
-			for(int j=1; j<N+1; j++) {
-				arr[j]=Integer.parseInt(st.nextToken());		        
+			for(int i=1; i<=N; i++) {
+				arr[i]=Integer.parseInt(st.nextToken());		        
 			}
-			
-			cnt=0;
-			for(int k=1; k<=N; k++) {
-				if(!visited[k]) {
-					list.clear();
-					dfs(k);
+			team=0; 
+			int group=0;
+			for(int i=1; i<=N; i++) {
+				if(!visited[i]) {
+					group++;
+					dfs(i, 1, group);
 				}
 			}
-			
-			sb.append(N-cnt+"\n");
+			sb.append(N-team+"\n");
 		}
-		System.out.println(sb.toString());
+		System.out.println(sb);
 	}
 
-	private static void dfs(int k) {
-		if (!visited[k]) {
-			visited[k] = true;
-			list.add(k);
-			if (list.contains(arr[k])) {
-				cnt+=list.size()-list.indexOf(arr[k]);
+	private static void dfs(int k,int cnt,int group) {
+		if(!visited[k]) {
+			visited[k]=true;
+			if(arr[k]==k) {
+				team++;
+				return;
 			}
-			else dfs(arr[k]);
+			if(cycle[0][arr[k]]!=0 && cycle[1][arr[k]]==group) {
+				team += cnt-cycle[0][arr[k]]+1;
+				return;
+			}
+			else {
+				cycle[0][k] = cnt++;
+				cycle[1][k] = group;
+				if(visited[arr[k]]) return;
+				dfs(arr[k],cnt,group);
+			}
 		}
 	}
 }
