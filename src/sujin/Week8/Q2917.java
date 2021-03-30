9299b2755a70fd3540277431ff74649fd5332e5f
SujinJeong <64065318+SujinJeong@users.noreply.github.com>
2021-03-30 23:54:04 +0900

    Q2917 사과나무 -- 수진

---
+++ src/sujin/Week8/Q2917.java
@@ -0,0 +1,23 @@
+package sujin.Week8;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q2917 {
+
+	static int[] dx = {1, 0, -1, 0};
+	static int[] dy = {0, 1, 0, -1};
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int[][] map = new int[n][m];
+		
+	}
+
+}
