f1a54f57b538d3de1bbeaec065203a50f9a06ca4
SujinJeong <64065318+SujinJeong@users.noreply.github.com>
2021-04-13 16:50:19 +0900

    4/13

--- src/sujin/Greedy/Q12845.java
+++ src/sujin/Greedy/Q12845.java
@@ -8,7 +8,6 @@ import java.util.Collections;
 
 /*
 1. 내림차순으로 정렬해서 큰 곳부터 작은 곳으로 합치기
-
  */
 public class Q12845 {
 
@@ -24,10 +23,9 @@ public class Q12845 {
 		Arrays.sort(num, Collections.reverseOrder());
 		
 		int gold = 0;
-		int level = num[0];
 		// 인덱스 작은거 -> 큰거로 덧붙여야 max
 		for (int i = 1; i < n; i++) {
-			gold += level + num[i];
+			gold += num[0] + num[i];
 		}
 		
 		System.out.println(gold);
--- src/sujin/Stack/Q5002.java
+++ src/sujin/Stack/Q5002.java
@@ -9,6 +9,7 @@ public class Q5002 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		int maxDiff = Integer.parseInt(br.readLine());
 		String order = br.readLine();
 		Stack<Character> s = new Stack<>();
@@ -16,33 +17,22 @@ public class Q5002 {
 		
 		int pair = 0;
 		for(int i = 0; i < order.length(); i++) {
-			// n+1까지만 기회있으므로 n+2는 가망없음
 			if (s.size() == (maxDiff+2)) {
 				isFinish = false;
 				break;
 			}
-			
-			char cur = order.charAt(i);
-			if (s.size() != 0 && s.peek() != cur) {
-				s.pop(); 
-				pair +=2;
+			if (s.size() != 0 && s.peek() != order.charAt(i)) {
+				s.pop(); pair +=2;
 				continue;
 			}
-			
-			// 두번째사람까지 들여보낼 수 있으므로 n+1 사람까지 기회 주는거임
-			// w,m 인 경우나 size가 n+2된경우 아니면 push
-			s.push(cur);
+			s.push(order.charAt(i));
 		}
 		
-		// 끝까지 간 경우에는 스택에 쌓여있는만큼 더해주기
-		if (isFinish)
-			if (s.size() > maxDiff)
-				System.out.println(pair+maxDiff);
-			else
-				System.out.println(pair+s.size());
-		// 중간에 끝난경우에는 pair수 + max 차이
-		else
-			System.out.println(pair+maxDiff);
+		if (isFinish && s.size() <= maxDiff)
+				sb.append(pair+s.size());
+		else sb.append(pair+maxDiff);
+		
+		System.out.println(sb);
 	}
 
 }
